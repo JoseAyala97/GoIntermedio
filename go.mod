5fe63adb53c5071f52300407986770a2723a52e3
joseayala <joseayala45@outlook.es>
2023-11-27 19:31:58 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoseAyala97/GoIntermedio
+
+go 1.21.4
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+// Primera letra en mayuscula para manejar encapsulamiento publico
+func HelloWord() {
+	fmt.Println("Hello world from Platzi!")
+}
